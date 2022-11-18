ad4f75ec0484755539478bbee68250262fa214d0
Pankaj Solanki <96578349+Pankajsolanki51@users.noreply.github.com>
2022-11-18 18:16:21 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import  ReactDOM from "react-dom";
+import "./state.css";
+import App from "./App";
+import GoogleApp from "../../src/GoogleApp";
+import ToDolist from "./ToDolist";
+import CompA from "./CompA";
+ReactDOM.render(<App/>, document.getElementById("root"));
